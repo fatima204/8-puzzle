81e373e4c710592b708648fb1e902547f603747e
Are <maksimovic34@gmail.com>
2023-12-22 11:59:44 +0100

    new branch

--- main.py
+++ main.py
@@ -1,16 +1,76 @@
-# This is a sample Python script.
-
-# Press Umschalt+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import random
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Strg+F8 to toggle the breakpoint.
+class Node:
+    board = [["_", 1, 2],
+             [3, 4, 5],
+             [6, 7, 8]]
+    g = 0
+    h = 0
+    f = 0
 
 
-# Press the green button in the gutter to run the script.
+# creates a shuffled initial node
+def initialNode():
+    node = Node()
+    flat = [element for sublist in node.board for element in sublist]  # flatten nested list using list comprehension
+    random.shuffle(flat)  # shuffle the flattened list
+    node.board = [flat[i:i + 3] for i in range(0, 9, 3)]  # recompose into nested list
+    return node
+
+
+# checks inversion count of given node
+def solvable(node):
+    count = 0
+    flat = [element for sublist in node.board for element in sublist]
+
+    for x in range(len(flat) - 1):
+        for y in range(x, len(flat)):
+            if flat[x] == "_" or flat[y] == "_":
+                continue  # ignores the blank space
+            if flat[x] > flat[y]:
+                count += 1
+
+    if count % 2 == 0:  # if inversion count is even = solvable
+        return True
+
+
+# prints the board of given node
+def printNode(node):
+    for x in range(3):
+        for y in range(3):
+            print(node.board[x][y], end=" ")
+        print("")
+
+
+# returns the blank space of given board
+def findBlank(node):
+    for x in range(3):
+        for y in range(3):
+            if node.board[x][y] == "_":
+                return x, y
+
+
+# counts the number of misplaced tiles
+def hamming(node):
+    distance = 0
+    goal = [["_", 1, 2],
+            [3, 4, 5],
+            [6, 7, 8]]
+
+    for x in range(3):
+        for y in range(3):
+            if node.board[x][y] != "_" and node.board[x][y] != goal[x][y]:
+                distance += 1
+
+    node.h = distance  # saves hamming heuristic of node
+    node.f = node.g + node.h  # updates cost of path
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    # testing
+    initial = initialNode()
+    if solvable(initial):
+        printNode(initial)
+        hamming(initial)
